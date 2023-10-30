631afeb252513ae7504a53ea3f8845d8777e2f14
PeterJung7733 <149376137+PeterJung7733@users.noreply.github.com>
2023-10-30 19:37:44 +0900

    Add files via upload
    첫 java 공부

---
+++ JavaChip.java
@@ -0,0 +1,8 @@
+class Javachip{
+public static void main(String[] args){
+System.out.println("Son heung min");
+System.out.println("Bong jun ho");
+System.out.println("Jung jei wook");
+System.out.println("let's go");
+}
+}
